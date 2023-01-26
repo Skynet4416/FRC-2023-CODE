636482c834f16e9e75b0c5089de1d5d60cec42f2
JoniNordi <52534955+YonaNord@users.noreply.github.com>
2023-01-26 23:34:04 +0200

    Added OI to Development...

---
+++ FRC-2023-CODE/src/main/java/frc/robot/OI.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+
+public class OI {
+        public static final Joystick rightJoystickController = new Joystick(
+                        frc.robot.Constants.OI.kRightJoystickControllerPort);
+        public static final Joystick leftJoystickController = new Joystick(
+                        frc.robot.Constants.OI.kLeftJoystickControllerPort);
+        public static final XboxController xboxController = new XboxController(
+                        frc.robot.Constants.OI.kXboxControllerPort);
+
+}
