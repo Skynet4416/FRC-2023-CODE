53e51928f5f0c8370c360495aefba334b4d71877
avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
2023-02-11 16:51:13 +0200

    Added functions
    Added acceleration and velocity functions that are needed to be tested.

--- FRC-2023-CODE/src/main/java/frc/robot/commands/drive/Autos.java
+++ FRC-2023-CODE/src/main/java/frc/robot/commands/drive/Autos.java
@@ -12,8 +12,15 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Timer;
+import com.kauailabs.navx.frc.AHRS;
+
+
 public final class Autos {
   /** Example static factory for an autonomous command. */
+  private AHRS AHRSsensor;
+  private double previousTime = 0;
   public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
     return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
   }
@@ -24,6 +31,32 @@ public final class Autos {
   }
 
   private Autos() {
+    //the roborio uses the spi protocol to give the "Maximal Performance" port for the ahrs sensor
+    AHRSsensor = new AHRS(SPI.Port.kMXP);
     throw new UnsupportedOperationException("This is a utility class!");
   }
+
+  /*public Autos(){
+    //the roborio uses the spi protocol to give the "Maximal Performance" port for the ahrs sensor
+    AHRSsensor = new AHRS(SPI.Port.kMXP);
+  }*/
+
+  public double GetAvrageAcceleration(){
+    //i used pethagoras theoroum to determine the final acceleration
+    //note: i am not sure how does it work in navex so if it doesn't work chage "getWorldLinearAccelZ" to "getWorldLinearAccelY" and
+    //i the function is times 9.80665^2 because it says the the "gravity component is removed" in the documantation and
+    //the data is returned in G force and needed to be converted to m/s^2
+    double acceleration = Math.pow(AHRSsensor.getWorldLinearAccelX() *9.80665*9.80665, 2) + Math.pow(AHRSsensor.getWorldLinearAccelZ()*9.80665*9.80665, 2);
+    return acceleration;
+  }
+
+  public double GetAvrageVelocity(){
+    double currentTime = Timer.getFPGATimestamp();
+    double deltaTime = currentTime - previousTime;
+    previousTime = currentTime;
+    //need to be completed
+    return 0;
+  }
+
+
 }
